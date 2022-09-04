51d0ace507bb5353ed9eecf92cca7c16944cd85e
BaoTrung37 <baotrung372k@gmail.com>
2022-09-05 02:35:58 +0700

    add rule

--- lib/presentation/utilities/initialize.dart
+++ lib/presentation/utilities/initialize.dart
@@ -1,5 +1,5 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:batru_house_rental/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class Initialize {
   static Future<void> initializationSetup() async {
