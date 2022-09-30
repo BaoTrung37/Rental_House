07d8838a63c7b00d345eccc8591a566f4653dd94
BaoTrung37 <baotrung372k@gmail.com>
2022-10-01 01:40:29 +0700

    add info view

--- lib/presentation/pages/post_article/post_article_view.dart
+++ lib/presentation/pages/post_article/post_article_view.dart
@@ -3,6 +3,8 @@ import 'package:batru_house_rental/presentation/pages/post_article/post_article_
 import 'package:batru_house_rental/presentation/resources/resources.dart';
 import 'package:batru_house_rental/presentation/widgets/base_app_bar/base_app_bar.dart';
 import 'package:batru_house_rental/presentation/widgets/buttons/app_button.dart';
+import 'package:batru_house_rental/presentation/widgets/input_text_field/input_text_field.dart';
+import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -93,8 +95,8 @@ class _PostArticleViewState extends ConsumerState<PostArticleView> {
             ),
             Step(
               state: setStepState(1),
-              title: const Text('Bước 2'),
-              content: const Text('Bước 2'),
+              title: const Text('Địa chỉ'),
+              content: _buildLocationInputView(),
             ),
             Step(
               state: setStepState(2),
@@ -112,10 +114,110 @@ class _PostArticleViewState extends ConsumerState<PostArticleView> {
     );
   }
 
+  Widget _buildLocationInputView() {
+    final list = <String>['Hà Đông', 'Cầu Giấy', 'Hoàn Kiếm', 'Tây Hồ'];
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          'Địa chỉ',
+          style: AppTextStyles.headingSmall,
+        ),
+        DropdownSearch<String>(
+          popupProps: const PopupProps.menu(
+            showSelectedItems: true,
+          ),
+          items: const ['Hà Đông', 'Cầu Giấy', 'Hoàn Kiếm', 'Tây Hồ'],
+          dropdownDecoratorProps: const DropDownDecoratorProps(
+            dropdownSearchDecoration: InputDecoration(
+              labelText: 'Quận/Huyện',
+              hintText: 'Bấm để chọn quận/huyện',
+            ),
+          ),
+          onChanged: print,
+          selectedItem: 'Hà Đông',
+        ),
+        DropdownSearch<String>(
+          popupProps: const PopupProps.menu(
+            showSelectedItems: true,
+          ),
+          items: const ['Phường DD', 'Phường DDd', 'Phường 23', 'Phường 2333'],
+          dropdownDecoratorProps: const DropDownDecoratorProps(
+            dropdownSearchDecoration: InputDecoration(
+              labelText: 'Phường/Xã',
+              hintText: 'Bấm để chọn phường/xã',
+            ),
+          ),
+          onChanged: print,
+          selectedItem: 'Phường DD',
+        ),
+        const SizedBox(height: 8),
+        const InputTextField(
+          labelText: 'Số nhà',
+          placeholder: 'Địa chỉ nhà',
+        ),
+        // DropdownSearch<UserModel>(
+        //   dropdownSearchDecoration: const InputDecoration(labelText: 'Name'),
+        //   asyncItems: (String filter) async {
+        //     final response = await Dio().get(
+        //       'http://5d85ccfb1e61af001471bf60.mockapi.io/user',
+        //       queryParameters: {'filter': filter},
+        //     );
+        //     final models = UserModel.fromJsonList(response.data);
+        //     return models;
+        //   },
+        //   onChanged: (UserModel? data) {
+        //     print(data);
+        //   },
+        // )
+      ],
+    );
+  }
+
   Widget _buildInfomationInputView() {
     return Column(
-      children: const [
-        Text('Bước 1'),
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          'Thông tin phòng',
+          style: AppTextStyles.headingSmall,
+        ),
+        const SizedBox(height: 8),
+        DropdownSearch<String>(
+          popupProps: const PopupProps.menu(
+            constraints: BoxConstraints(
+              maxHeight: 220,
+            ),
+            showSelectedItems: true,
+            fit: FlexFit.loose,
+          ),
+          items: const [
+            'Phòng cho thuê',
+            'Home stay',
+            'Nhà nguyên căn',
+            'Căn hộ',
+          ],
+          dropdownDecoratorProps: const DropDownDecoratorProps(
+            dropdownSearchDecoration: InputDecoration(
+              labelText: 'Loại phòng',
+              hintText: 'Bấm để chọn loại phòng',
+            ),
+          ),
+          onChanged: print,
+          selectedItem: 'Phòng cho thuê',
+        ),
+        const SizedBox(height: 8),
+        const InputTextField(
+          labelText: 'Số lượng phòng (phòng)',
+          placeholder: 'Số lượng phòng bạn đang quản lý',
+          keyboardType: TextInputType.number,
+        ),
+        const SizedBox(height: 8),
+        const InputTextField(
+          labelText: 'Sức chứa (người)',
+          placeholder: 'Số lượng người tối đa có thể chứa',
+          keyboardType: TextInputType.number,
+        ),
       ],
     );
   }
