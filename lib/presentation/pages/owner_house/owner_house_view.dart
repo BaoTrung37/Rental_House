90d10e03b62845e43d8017ff6cc4f5b399ab4893
mvn-baotrung-hn <trung.nguyen4@monstar-lab.com>
2022-11-21 14:45:05 +0700

    refactor code

--- lib/presentation/pages/owner_house/owner_house_view.dart
+++ lib/presentation/pages/owner_house/owner_house_view.dart
@@ -1,6 +1,5 @@
 import 'package:batru_house_rental/data/providers/app_navigator_provider.dart';
 import 'package:batru_house_rental/domain/use_case/article/get_articles_by_user_id_use_case.dart';
-import 'package:batru_house_rental/domain/use_case/auth/get_current_user_information_use_case.dart';
 import 'package:batru_house_rental/injection/injector.dart';
 import 'package:batru_house_rental/presentation/navigation/app_routers.dart';
 import 'package:batru_house_rental/presentation/pages/house_detail/house_detail_view.dart';
--- lib/presentation/pages/owner_house/owner_house_view_model.dart
+++ lib/presentation/pages/owner_house/owner_house_view_model.dart
@@ -1,5 +1,4 @@
 import 'package:batru_house_rental/domain/use_case/article/get_articles_by_user_id_use_case.dart';
-import 'package:batru_house_rental/domain/use_case/auth/get_current_user_information_use_case.dart';
 import 'package:batru_house_rental/presentation/pages/owner_house/owner_house_state.dart';
 import 'package:batru_house_rental/presentation/utilities/enums/loading_status.dart';
 import 'package:flutter/material.dart';
