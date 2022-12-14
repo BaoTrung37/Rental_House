143cf0061ce5f55db26b4fb8c248ae9a2f35a64b
BaoTrung37 <baotrung372k@gmail.com>
2022-12-15 01:38:05 +0700

    remove dummy link

--- lib/presentation/pages/home/home_view.dart
+++ lib/presentation/pages/home/home_view.dart
@@ -34,8 +34,7 @@ class HomeView extends ConsumerStatefulWidget {
 }
 
 class _HomeViewState extends ConsumerState<HomeView> {
-  final mockThumbnail =
-      'https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg';
+  
 
   HomeViewModel get _viewModel => ref.read(homeViewProvider.notifier);
   HomeState get _state => ref.watch(homeViewProvider);
