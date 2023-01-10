c93b906095b4b85043808ba1d15304b6a8ce8412
BaoTrung37 <baotrung372k@gmail.com>
2023-01-10 17:56:11 +0700

    refactor code

--- lib/presentation/pages/admin_report/admin_report_view.dart
+++ lib/presentation/pages/admin_report/admin_report_view.dart
@@ -1,3 +1,4 @@
+import 'package:batru_house_rental/presentation/resources/resources.dart';
 import 'package:batru_house_rental/presentation/widgets/base_app_bar/base_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -13,10 +14,29 @@ class AdminReportView extends ConsumerStatefulWidget {
 class _AdminReportViewState extends ConsumerState<AdminReportView> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: BaseAppBar.titleOnly(title: 'Quản lý báo cáo'),
-      body: Center(
-        child: Text(''),
+    return Scaffold(
+      appBar: const BaseAppBar.titleOnly(title: 'Quản lý báo cáo'),
+      body: Container(
+        margin: const EdgeInsets.all(8),
+        padding: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: context.colors.action,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: const [
+            Text(
+              'Người dùng: hahahâhahahah đã báo cáo một bài đăng của người dùng: hiahiaihahiaih',
+              style: AppTextStyles.headingXSmall,
+            ),
+            SizedBox(height: 8),
+            Text('Lý do: ', style: AppTextStyles.textMedium),
+            SizedBox(height: 8),
+            Text('Ngày tạo: ', style: AppTextStyles.textMedium),
+          ],
+        ),
       ),
     );
   }
